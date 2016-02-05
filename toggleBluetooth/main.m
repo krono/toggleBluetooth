daf9df03f0440b6a833b04ab8600ac39f997990c
Tobias Pape <tobias@netshed.de>
2016-02-05 09:04:46 +0100

    Initial Commit

---
+++ toggleBluetooth/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  toggleBluetooth
+//
+//  Created by Tobias Pape on 05.02.16.
+//  Copyright (c) 2016 Tobias Pape. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
